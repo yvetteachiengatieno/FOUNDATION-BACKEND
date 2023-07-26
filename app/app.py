8f13c5b07902e5d556c61cd92a401873afdc6a60
yvetteachiengatieno <ivet.achieng@code.berlin>
2023-07-26 15:46:11 +0200

    flask migrate

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, redirect, url_for, render_template
 from . import merchandise, simple_pages
-from app.extensions.database import db
+from app.extensions.database import db, migrate
 
 
 
@@ -9,6 +9,7 @@ app.config.from_object('app.config')
 app.register_blueprint(merchandise.routes.blueprint)
 app.register_blueprint(simple_pages.routes.blueprint)
 db.init_app(app)
+migrate.init_app(app,db, compare_type=True)
 
 
 
